1789a7653adf5b0c0f54d41f9ba5e3163b92555f
Ben Willemsma <ben.willemsma@gmail.com>
2018-10-18 17:33:12 -0400

    PoolTable

---
+++ Assets/HauntedHouse/Scripts/Objects/PoolBall.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PoolBall : MonoBehaviour
+{
+    public Transform boundryCenter;
+    public float boundryDistance = 4;
+
+    private Rigidbody rb;
+    private Vector3 startPos;
+
+	void Start ()
+    {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void Update()
+    {
+        if(boundryCenter && (transform.position - boundryCenter.position).magnitude > boundryDistance)
+            rb.velocity = Vector3.zero + Vector3.up * rb.velocity.y;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody other = collision.gameObject.GetComponent<Rigidbody>();
+        if (other)
+        {
+            Vector3 normal = collision.contacts[0].normal;
+            float force = Vector3.Project(rb.velocity, normal).magnitude * 0.8f;
+            other.AddForce(normal * -force, ForceMode.Impulse);
+            other.AddForce(normal * force, ForceMode.Impulse);
+        }
+        else rb.velocity = Vector3.Reflect(rb.velocity, collision.contacts[0].normal);
+    }
+
+    public void ResetBall()
+    {
+        transform.position = startPos;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/HauntedHouse/Scripts/Objects/PoolTable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolTable : MonoBehaviour
+{
+    public List<PoolBall> balls;
+    public int[] BallOrder = new int[16];
+
+    public GameObject key;
+
+    private int orderIndex = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PoolBall ball;
+        if (ball = other.GetComponent<PoolBall>())
+        {
+            if (balls.IndexOf(ball) + 1 == BallOrder[orderIndex])
+            {
+                ball.gameObject.SetActive(false);
+                orderIndex++;
+                if (orderIndex >= BallOrder.Length)
+                    RewardKey();
+            }
+            else
+            {
+                for (int i = 0; i < balls.Count; i++)
+                    balls[i].ResetBall();
+            }
+            
+        }
+    }
+
+    private void RewardKey()
+    {
+        if (key) key.SetActive(true);
+        Debug.Log("Key Rewarded");
+    }
+}
