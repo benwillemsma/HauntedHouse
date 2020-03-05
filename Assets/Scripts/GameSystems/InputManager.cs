ba4d93b900b6f57d49c75bcf768a19261c4a1be8
Ben Willemsma <ben.willemsma@gmail.com>
2020-03-04 22:17:05 -0500

    Restart

---
+++ Assets/Scripts/GameSystems/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class InputManager : MonoBehaviour
+{
+    public Transform head;
+    public Rigidbody body;
+    public float moveSpeed = 1.2f;
+
+    private void Update()
+    {
+        if (body && head)
+        {
+            Vector3 moveDir = Vector3.zero;
+            Debug.Log("moveDirection:" + moveDir);
+            moveDir = Quaternion.AngleAxis(head.eulerAngles.y, Vector3.up) * new Vector3(moveDir.x, 0, moveDir.y);
+            body.velocity = (moveDir * moveSpeed) + (Vector3.up * body.velocity.y);
+            Debug.Log("Velocity:" + moveDir);
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Safe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Safe : MonoBehaviour
+{
+    public Rigidbody door;
+    private bool Locked = true;
+
+    public void Open()
+    {
+        if (Locked)
+        {
+            Locked = false;
+            if (door)
+            {
+                door.AddForce(transform.right * 5, ForceMode.Impulse);
+                //door.GetComponent<CircularDrive>().minAngle = -180;
+            }
+        }
+    }
+}
